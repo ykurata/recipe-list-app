3fb733ca549dd6709e8ce2a78232bb6f92813ab6
Yasuko Kurata <yasukokurata@gmail.com>
2021-02-15 22:32:21 -0500

    add recipeFeaturedView

--- Recipe List App/Views/RecipeTabView.swift
+++ Recipe List App/Views/RecipeTabView.swift
@@ -11,7 +11,7 @@ struct RecipeTabView: View {
     var body: some View {
         
         TabView {
-            Text("Featured View")
+            RecipeFeaturedView()
                 .tabItem {
                     VStack {
                         Image(systemName: "star.fill")
@@ -27,6 +27,7 @@ struct RecipeTabView: View {
                     }
                 }
         }
+        .environmentObject(RecipeModel())
     }
 }
 
